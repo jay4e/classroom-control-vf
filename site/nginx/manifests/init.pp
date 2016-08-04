56e8fd48bf6f6ecccb49ec6494405c4f32e706c6
jay4e <jay@lentecs.com>
2016-08-04 14:35:23 -0400

    15.2.0

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,74 @@
 
 class nginx {
   
-  package { 'nginx': ensure => present }
-  
+  case $family {
+    'Redhat': {
+       $package = 'nginx'
+       $owner = 'root'
+       $group = 'root'
+       $root_dir = ''
+       $document_dir = 'var/www'
+       $conf_dir = 'etc/nginx'
+       $log_dir = 'var/log/nginx'
+       $service_name = 'nginx'
+       $service_user = 'nginx'
+    }
+    'Debian': {
+       $package = 'nginx'
+       $owner = 'root'
+       $group = 'root'
+       $root_dir = ''
+       $document_dir = 'var/www'
+       $conf_dir = 'etc/nginx'
+       $log_dir = 'var/log/nginx'
+       $service_name = 'nginx'
+       $service_user = 'nginx'
+    }
+    'Windows': {
+       $package = 'nginx-service'
+       $owner = 'Administrator'
+       $group = 'Administor'
+       $root_dir = 'C:/ProgramData'
+       $document_dir = 'nginx/html'
+       $conf_dir = 'nginx'
+       $log_dir = 'nginx/logs'
+       $service_name = 'nginx'
+       $service_user = 'nobody'
+    }
+       
+  package { $package: ensure => present }
+       
   File {
-    owner  => 'root',
-    group => 'root',
+    owner  => $owner,
+    group => $group,
     mode  => '0664'
   }
   
-  file { '/var/www':
+  file { "$root_dir/$document_dir":
     ensure => 'directory',
   }
-  file { '/var/www/index.html':
+  file { "$root_dir/$document_dir/index.html":
     ensure    => 'file',
     source    => 'puppet:///modules/nginx/index.html',
   }
   
-  file { '/etc/nginx/nginx.conf':
+  file { "$root_dir/$conf_dir/nginx.conf":
     ensure    => 'file',
     source    => 'puppet:///modules/nginx/nginx.conf',
-    require   => Package['nginx'],
-    notify    => Service['nginx'],
+    require   => Package[$package],
+    notify    => Service[$service_name],
   }
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "$root_dir/$conf_dir/conf.d/default.conf":
     ensure    => 'file',
     source    => 'puppet:///modules/nginx/default.conf',
-    require   => Package['nginx'],
-    notify    => Service['nginx'],
+    require   => Package[$package],
+    notify    => Service[$service_name],
   }
   
-  service { 'nginx':
+  service { '$service_name:
     ensure     => 'running',
     enable     => 'true',
     hasrestart => 'true',
-    require    => Package['nginx']
+    require    => Package[$package]
   }
 }
