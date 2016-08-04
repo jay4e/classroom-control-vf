1570812e995f6c096008620dbee4003619f07cfc
jay4e <jay@lentecs.com>
2016-08-04 08:39:37 -0400

    Exercise 13.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,34 +3,28 @@ class nginx {
   
   package { 'nginx': ensure => present }
   
-  file { '/var/www':
-    ensure => 'directory',
+  File {
     owner  => 'root',
     group => 'root',
     mode  => '0644'
   }
+  
+  file { '/var/www':
+    ensure => 'directory',
+  }
   file { '/var/www/index.html':
     ensure    => 'file',
-    mode      => '644',
-    owner     => 'root',
-    group     => 'root',
     source    => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure    => 'file',
-    mode      => '644',
-    owner     => 'root',
-    group     => 'root',
     source    => 'puppet:///modules/nginx/nginx.conf',
     require   => Package['nginx'],
     notify    => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure    => 'file',
-    mode      => '644',
-    owner     => 'root',
-    group     => 'root',
     source    => 'puppet:///modules/nginx/default.conf',
     require   => Package['nginx'],
     notify    => Service['nginx'],
