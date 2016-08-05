cc8755bfa01c78800f2e7248240a47c67b1d1895
jay4e <jay@lentecs.com>
2016-08-05 11:20:05 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,10 +1,10 @@
 
 class nginx( 
+       $document_dir,
        $package = $nginx::params::package,
        $owner = $nginx::params::owner,
        $group = $nginx::params::group,
        $root_dir = $nginx::params::root_dir,
-       $document_dir,
        $conf_dir = $nginx::params::conf_dir,
        $log_dir = $nginx::params::log_dir,
        $service_name = $nginx::params::service_name,
